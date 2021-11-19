61e2a96d01bea068765f0f501f83308705706d78
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-11-19 02:21:13 +0100

    function createTask created

--- JS_Files/addTask.js
+++ JS_Files/addTask.js
@@ -1,12 +1,22 @@
 let allTasks = [];
 
 function createTask() {
+    let title = document.getElementById('inputTitle').value;
+    let description = document.getElementById('description').value;
+    let priortity = document.getElementById('priortity').value;
+    let expirationDate = document.getElementById('expirationDate').value;
+    let creator = document.getElementById('creator').value;
+   let createdAt = new Date().getTime();
 
-}
+    let task = {
+        'title': title,
+        'description': description,
+        'priortity': priortity,
+        'expirationDate': expirationDate,
+        'creator': creator,
+      'createdAt': createdAt
+    };
 
-
-
-
-function getInput() {
-    
+    allTasks.push(task);
+    console.log(allTasks);
 }
