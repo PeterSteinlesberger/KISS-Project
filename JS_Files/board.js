9771aab25d4fedc7fcde81584379b4bd1ce89f3b
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-12-01 14:40:37 +0100

    board todo structre finished

--- JS_Files/board.js
+++ JS_Files/board.js
@@ -1,6 +1,24 @@
 function showTodo() {
-    let todoContainer = document.getElementById('todo');
+    let todoContainer = document.getElementById('categoryTodo');
 
+
+
+    <div id="todo" class="todo">
+    <div class="urgency"></div>
+    <div>
+        <div class="todo-cont ">
+            <h3 id="todoTitle" class="todo-title">12345678901234567890</h3>
+            <span id="finalDay" class="final-day">27.03.2021</span>
+        </div>
+        <div class="todo-cont">
+            <div class="descritpion-cont">
+                <span id="todo-description" class="todo-description"></span>
+            </div>
+            <div class="trash-img"><img src=./img/behalter.png class="deleteImg"
+                    onclick="deleteTask(${i})"></div>
+        </div>
+    </div>
+</div>
 
 
     
