267efde40b0b90ac5d04c56e22ea718a8cd4bf50
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-12-08 13:02:26 +0100

    showList functions for board added

--- JS_Files/board.js
+++ JS_Files/board.js
@@ -15,7 +15,6 @@ function showTodos() {
     let todoContainer = document.getElementById('todo');
     todoContainer.innerHTML = '';
 
-
     for (let i = 0; i < activeTasks.length; i++) {
         const task = activeTasks[i];
         let urgencyColor = activeTasks[i]['priortity'];
@@ -68,69 +67,134 @@ function drag(taskID) {
 
 async function drop(listName) {
     let taskToMove = activeTasks.find(task => task.taskId === currentDraggedElement);
-taskToMove.list = listName;
-await backend.setItem('allTasks', JSON.stringify(allTasks));
-showTodos();
+    taskToMove.list = listName;
+    await backend.setItem('allTasks', JSON.stringify(allTasks));
+    showTodos();
 }
-/*
-function updateHMTL() {
 
-    let todo = todos.filter(t => !t['list'] || t['list'] == 'toDo');
-
-    document.getElementById('toDo').innerHTML = '';
-
-    for (let index = 0; index < todo.length; index++) {
-        const element = todo[index];
-        document.getElementById('toDo').innerHTML += `<div draggable="true" ondragstart="startDragging(${element.createdAt})" class="todo">${todo[index].taskDescription}
-        </div>`;
-    }
+/* Board - updateHTML */
 
 
-    let inProgress = todos.filter(t => t['list'] == 'inProgress');
+function showTodoList() {
+    let list = activeTasks.filter(task => task.list === 'todo');
+    let listContainer = document.getElementById('todo');
+    listContainer.innerHTML = '';
 
-    document.getElementById('inProgress').innerHTML = '';
+    for (let i = 0; i < list.length; i++) {
+        const task = list[i];
+        let urgencyColor = list[i]['priortity'];
 
-    for (let index = 0; index < inProgress.length; index++) {
-        const element = inProgress[index];
-        document.getElementById('inProgress').innerHTML += `<div draggable="true" ondragstart="startDragging(${element.createdAt})" class="todo">${inProgress[index].taskDescription}
-        </div>`;
-    }
-
-
-    let testing = todos.filter(t => t['list'] == 'Testing');
-
-    document.getElementById('Testing').innerHTML = '';
-
-    for (let index = 0; index < testing.length; index++) {
-        const element = testing[index];
-        document.getElementById('Testing').innerHTML += `<div draggable="true" ondragstart="startDragging(${element.createdAt})" class="todo">${testing[index].taskDescription}
-        </div>`;
-    }
-
-
-    let done = todos.filter(t => t['list'] == 'Done');
-
-    document.getElementById('Done').innerHTML = '';
-
-    for (let index = 0; index < done.length; index++) {
-        const element = done[index];
-        document.getElementById('Done').innerHTML += `<div draggable="true" ondragstart="startDragging(${element.createdAt})" class="todo">${done[index].taskDescription}
-        </div>`;
+        listContainer.innerHTML += `
+      <div id="${task.taskId}" class="todo" draggable="true" ondragstart="drag(${task.taskId})">
+    <div class="urgency ${urgencyColor}"></div>
+    <div>
+        <div class="todo-cont ">
+            <h3 class="todo-title">${task['title']}</h3>
+            <span class="final-day">${task['expirationDate']}</span>
+        </div>
+        <div class="todo-cont">
+            <div class="descritpion-cont">
+                <span class="todo-description">${task['description']}</span>
+            </div>
+            <div class="trash-img"><img src=./img/behalter.png class="deleteImg"
+                    onclick="deleteTask(${i})"></div>
+        </div>
+    </div>
+  </div>
+    `;
     }
 
 }
 
 
-function startDragging(createdAt) {
-    currentDraggedElement = createdAt; //!!
+function showInProgressList() {
+    let list = activeTasks.filter(task => task.list === 'inProgress');
+    let listContainer = document.getElementById('inProgress');
+    listContainer.innerHTML = '';
+
+    for (let i = 0; i < list.length; i++) {
+        const task = list[i];
+        let urgencyColor = list[i]['priortity'];
+
+        listContainer.innerHTML += `
+      <div id="${task.taskId}" class="todo" draggable="true" ondragstart="drag(${task.taskId})">
+    <div class="urgency ${urgencyColor}"></div>
+    <div>
+        <div class="todo-cont ">
+            <h3 class="todo-title">${task['title']}</h3>
+            <span class="final-day">${task['expirationDate']}</span>
+        </div>
+        <div class="todo-cont">
+            <div class="descritpion-cont">
+                <span class="todo-description">${task['description']}</span>
+            </div>
+            <div class="trash-img"><img src=./img/behalter.png class="deleteImg"
+                    onclick="deleteTask(${i})"></div>
+        </div>
+    </div>
+  </div>
+    `;
+    }
 }
 
 
-async function moveto(list) {
-    const task = todos.find(t => t.createdAt === currentDraggedElement);
-    task.list = list;
-    await backend.setItem('allTasks', JSON.stringify(todos));
-    updateHtml();
+function showDoneList() {
+    let list = activeTasks.filter(task => task.list === 'done');
+    let listContainer = document.getElementById('done');
+    listContainer.innerHTML = '';
 
+    for (let i = 0; i < list.length; i++) {
+        const task = list[i];
+        let urgencyColor = list[i]['priortity'];
+
+        listContainer.innerHTML += `
+      <div id="${task.taskId}" class="todo" draggable="true" ondragstart="drag(${task.taskId})">
+    <div class="urgency ${urgencyColor}"></div>
+    <div>
+        <div class="todo-cont ">
+            <h3 class="todo-title">${task['title']}</h3>
+            <span class="final-day">${task['expirationDate']}</span>
+        </div>
+        <div class="todo-cont">
+            <div class="descritpion-cont">
+                <span class="todo-description">${task['description']}</span>
+            </div>
+            <div class="trash-img"><img src=./img/behalter.png class="deleteImg"
+                    onclick="deleteTask(${i})"></div>
+        </div>
+    </div>
+  </div>
+    `;
+    }
 }
-*/
+
+
+function showNotDoneList() {
+    let list = activeTasks.filter(task => task.list === 'notDone');
+    let listContainer = document.getElementById('notDone');
+    listContainer.innerHTML = '';
+
+    for (let i = 0; i < list.length; i++) {
+        const task = list[i];
+        let urgencyColor = list[i]['priortity'];
+
+        listContainer.innerHTML += `
+      <div id="${task.taskId}" class="todo" draggable="true" ondragstart="drag(${task.taskId})">
+    <div class="urgency ${urgencyColor}"></div>
+    <div>
+        <div class="todo-cont ">
+            <h3 class="todo-title">${task['title']}</h3>
+            <span class="final-day">${task['expirationDate']}</span>
+        </div>
+        <div class="todo-cont">
+            <div class="descritpion-cont">
+                <span class="todo-description">${task['description']}</span>
+            </div>
+            <div class="trash-img"><img src=./img/behalter.png class="deleteImg"
+                    onclick="deleteTask(${i})"></div>
+        </div>
+    </div>
+  </div>
+    `;
+    }
+}
