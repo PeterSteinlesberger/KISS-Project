8ffca15fb0937797274c8eec6428185eed93da50
PeterSteinlesberger <petersteinlesberger@mail.de>
2022-03-06 22:16:09 +0100

    Add: landingpage

--- JS_Files/script.js
+++ JS_Files/script.js
@@ -5,7 +5,7 @@ setURL('http://peter-steinlesberger.developerakademie.com/KISS-Project/smallest_
  
  async function init() {
   await includeHTML();
-  bindGoogleTranslator();
+ // bindGoogleTranslator();
 }
 // ------------------- new includeHTML function -----------------//
 
@@ -13,55 +13,55 @@ setURL('http://peter-steinlesberger.developerakademie.com/KISS-Project/smallest_
 /* Make an HTTP request using the attribute value as the file name: */
 /* Remove this attribute and call this function once more */
 /* Exit the function: */
-async function includeHTML() {
-  try {
-    let templateContainers = document.querySelectorAll('[data-template]');
-    for (let i = 0; i < templateContainers.length; i++) {
-      let elmnt = templateContainers[i];
-      let templatePath = elmnt.getAttribute('data-template');
-      let response = await fetch(templatePath);
-      if (response.status == 200) {
-        elmnt.innerHTML = await response.text();
-      } else {
-        elmnt.innerHTML = response.statusText;
-      }
-      elmnt.removeAttribute('data-template');
-      await includeHTML();
-      return;
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-// ------------------- old includeHTML function -----------------//
-//   function includeHTML() {
-//   var z, i, elmnt, file, xhttp;
-//   /*loop through a collection of all HTML elements:*/
-//   z = document.getElementsByTagName("*");
-//   for (i = 0; i < z.length; i++) {
-//     elmnt = z[i];
-//     /*search for elements with a certain atrribute:*/
-//     file = elmnt.getAttribute("w3-include-html");
-//     if (file) {
-//       /*make an HTTP request using the attribute value as the file name:*/
-//       xhttp = new XMLHttpRequest();
-//       xhttp.onreadystatechange = function () {
-//         if (this.readyState == 4) {
-//           if (this.status == 200) { elmnt.innerHTML = this.responseText; }
-//           if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
-//           /*remove the attribute, and call this function once more:*/
-//           elmnt.removeAttribute("w3-include-html");
-//           includeHTML();
-//         }
+// async function includeHTML() {
+//   try {
+//     let templateContainers = document.querySelectorAll('[data-template]');
+//     for (let i = 0; i < templateContainers.length; i++) {
+//       let elmnt = templateContainers[i];
+//       let templatePath = elmnt.getAttribute('data-template');
+//       let response = await fetch(templatePath);
+//       if (response.status == 200) {
+//         elmnt.innerHTML = await response.text();
+//       } else {
+//         elmnt.innerHTML = response.statusText;
 //       }
-//       xhttp.open("GET", file, true);
-//       xhttp.send();
-//       /*exit the function:*/
+//       elmnt.removeAttribute('data-template');
+//       await includeHTML();
 //       return;
 //     }
+//   } catch (error) {
+//     console.error(error);
 //   }
-//  }
+// }
+
+// ------------------- old includeHTML function -----------------//
+ async function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+          if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+ }
 
 
 async function  downloadData() {
@@ -82,22 +82,30 @@ async function deleteTask(position) {
 // ---------------------- google translate functions ----------------------//
 
 
-function bindGoogleTranslator() {
-  let script = document.createElement('script');
-  script.setAttribute('type', 'text/javascript');
-  script.setAttribute(
-    'src',
-    '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
-  );
-  document.body.insertAdjacentElement('beforeend', script);
-}
+// function bindGoogleTranslator() {
+//   let script = document.createElement('script');
+//   script.setAttribute('type', 'text/javascript');
+//   script.setAttribute(
+//     'src',
+//     '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
+//   );
+//   document.body.insertAdjacentElement('beforeend', script);
+// }
 
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement(
-    { pageLanguage: 'en', includedLanguages: 'en,es,de' },
-    'google_translate_element'
-  );
-}
+// function googleTranslateElementInit() {
+//   new google.translate.TranslateElement(
+//     { pageLanguage: 'en', includedLanguages: 'en,es,de' },
+//     'google_translate_element'
+//   );
+// }
 
 init();
 
+
+function openBackgroundMenu() {
+    
+}
+
+function openHelpSection() {
+  
+}
