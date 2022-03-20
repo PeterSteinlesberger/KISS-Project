70c5efc5bc388234e9461b4f8a527b2719b41a70
PeterSteinlesberger <petersteinlesberger@mail.de>
2022-03-20 23:50:06 +0100

    Feat: taskOnHover function in the board-section

--- JS_Files/backlog.js
+++ JS_Files/backlog.js
@@ -1,5 +1,5 @@
 let inactiveTasks = [];
- 
+
 
 async function initBacklog() {
     includeHTML();
@@ -17,8 +17,8 @@ function showBacklog() {
         let urgency = task['priortity'];
         backlogFile.innerHTML += `<div style="padding: 12px"> 
         <div class="backlog-file-container">
-        <div id="tooltip1" onmouseover="showTooltip('tooltip1')" onmouseleave="hideTooltip('tooltip1')" > 
-        <div id="backlogFile" class="backlog-file" onclick="setStatus(${task['taskId']})">
+        <div id="addTask" onmouseover="showTooltip('addTask')" onmouseleave="hideTooltip('addTask')" > 
+        <div id="task" class="backlog-file" onclick="setStatus(${task['taskId']})">
         <div class="${urgency}"></div>
         <img class="creator-img" src="${task['creatorImg']}" id="creatorImg">
         <div class="creator padding-top" id="creator">${task['creator']}</div>
@@ -29,48 +29,54 @@ function showBacklog() {
         <div id="addInfo" class="info-box">ADD TASK TO BOARD</div>
         </div> </div> 
         <div id="deleteImgContainer" class="delete-img-container">
-        <img id="tooltip2" onmouseover="showTooltip('tooltip2')" onmouseleave="hideTooltip('tooltip2')" src=./img/trash_icon.png class="deleteImg" onclick="deleteBacklogTask(${i})">
+        <img id="deleteTask" onmouseover="showTooltip('deleteTask')" onmouseleave="hideTooltip('deleteTask')" src=./img/trash_icon.png class="deleteImg" onclick="deleteBacklogTask(${i})">
         </div></div></div>`;
     }
 }
 
+
+
 function showTooltip(id) {
-    console.log(id);
     let addInfo = document.getElementById('addInfo');
     let deleteInfo = document.getElementById('deleteInfo');
+    let task = document.getElementById('task');
     let deleteImgContainer = document.getElementById('deleteImgContainer');
-    let trashImg = document.getElementById('tooltip2');
-    let toolTip1 = document.getElementById('tooltip1');
-    if(id == 'tooltip1') {
-      addInfo.style.display ="flex";
+    let trashImg = document.getElementById('deleteTask');
+    let addTask = document.getElementById('addTask');
+    if (id == 'addTask') {
+        addInfo.style.display = "flex";
+        deleteImgContainer.style.borderLeft = "none";
+    } else
+        if (id == 'deleteTask') {
+            deleteInfo.style.display = "flex";
+            deleteInfo.style.color = "red";
+            deleteImgContainer.style.border = "1px solid red";
+            deleteImgContainer.style.borderLeft = "none";
+            task.style.border = "1px solid red";
+            task.style.borderRight = "none";
+            trashImg.src = "./img/trash_icon_red.png";
+        }
 
-    } else 
-    if(id == 'tooltip2') {
-        deleteInfo.style.display ="flex";
-        deleteImgContainer.style.border ="1px solid red";
-        deleteImgContainer.style.borderLeft ="none";
-        // deleteImgContainer.classList.add('delete-red');
-        // toolTip1.classList.add('backlog-file-red');
-        trashImg.src ="./img/trash_icon_red.png";
-      }
- 
 }
 
 function hideTooltip(id) {
     let addInfo = document.getElementById('addInfo');
     let deleteInfo = document.getElementById('deleteInfo');
     let deleteImgContainer = document.getElementById('deleteImgContainer');
-    if(id == 'tooltip1') {
-      addInfo.style.display ="none";
-     } else 
-     if(id == 'tooltip2') {
-        deleteInfo.style.display ="none";
-        deleteImgContainer.style.border ="1px solid blue"; 
-        
-        let trashImg = document.getElementById('tooltip2');
-        trashImg.src ="./img/trash_icon.png";
-     } 
-      
+    let task = document.getElementById('task');
+    if (id == 'addTask') {
+        addInfo.style.display = "none";
+    } else
+        if (id == 'deleteTask') {
+            deleteInfo.style.display = "none";
+            deleteImgContainer.style.border = "solid 1px rgb(0, 114, 255)";
+            deleteImgContainer.style.borderLeft = "none";
+            task.style.border = "1px solid rgb(0, 114, 255)";
+            task.style.borderRight = "none";
+            let trashImg = document.getElementById('deleteTask');
+            trashImg.src = "./img/trash_icon.png";
+        }
+
 }
 
 
@@ -92,4 +98,3 @@ function filterInactiveTasks() {
     inactiveTasks = allTasks.filter(task => task.status === 'inactive');
 }
 
-   
