7570c1a093c6a0f732b17e8e642a2f3a9f3b10c8
PeterSteinlesberger <petersteinlesberger@mail.de>
2022-03-21 14:22:54 +0100

    Fix: board-section  show TaskInfo on the right task

--- JS_Files/backlog.js
+++ JS_Files/backlog.js
@@ -12,90 +12,103 @@ function showBacklog() {
     filterInactiveTasks();
     let backlogFile = document.getElementById('backlogFile');
     backlogFile.innerHTML = ``;
+    renderBacklog();
+}
+
+
+function renderBacklog() {
     for (let i = 0; i < inactiveTasks.length; i++) {
         let task = inactiveTasks[i];
         let urgency = task['priortity'];
         backlogFile.innerHTML += ` 
-        <div class="backlog-file-container">
-        <div id="addTask" onmouseover="showTooltip('addTask')" onmouseleave="hideTooltip('addTask')" > 
-        <div id="task" class="backlog-file" onclick="setStatus(${task['taskId']})">
+        <div id="addTask${i}" class="backlog-file-container">
+        <div  onmouseover="showTooltip('addTask${i}',  ${i})" onmouseleave="hideTooltip('addTask${i}', ${i})" > 
+        <div id="task${i}" class="backlog-file" onclick="setStatus(${task['taskId']})">
         <div class="${urgency}"></div>
-        <img class="creator-img" src="${task['creatorImg']}" id="creatorImg">
-        <div class="creator padding-top" id="creator">${task['creator']}</div>
-        <div class="title padding-top" id="title">${task['title']}</div>
-        <span class="description padding-top" id="description">${task['description']}</span>
-        <div class="finalday padding-top" id="finishDate">${task['expirationDate']}</div>
-        <div id="deleteInfo" class="info-box">DELETE TASK</div>
-        <div id="addInfo" class="info-box">ADD TASK TO BOARD</div>
+        <img id="creatorImg" class="creator-img" src="${task['creatorImg']}" >
+        <div id="creator" class="creator padding-top" >${task['creator']}</div>
+        <div id="title" class="title padding-top" >${task['title']}</div>
+        <span id="description" class="description padding-top" >${task['description']}</span>
+        <div id="finishDate" class="finalday padding-top" >${task['expirationDate']}</div>
+        <div id="deleteInfo${i}" class="info-box">DELETE TASK</div>
+        <div id="addInfo${i}" class="info-box">ADD TASK TO BOARD</div>
         </div> </div> 
-        <div id="deleteImgContainer" class="delete-img-container">
-        <img id="deleteTask" onmouseover="showTooltip('deleteTask')" onmouseleave="hideTooltip('deleteTask')" src=./img/trash_icon.png class="deleteImg" onclick="deleteBacklogTask(${i})">
+        <div id="deleteImgContainer${i}" class="delete-img-container">
+        <img id="deleteTask${i}" onmouseover="showTooltip('deleteTask${i}', ${i})" onmouseleave="hideTooltip('deleteTask${i}', ${i})" src=./img/trash_icon.png class="deleteImg" onclick="deleteBacklogTask(${i})">
         </div></div>`;
     }
 }
 
 
-
-function showTooltip(id) {
-    let addInfo = document.getElementById('addInfo');
-    let deleteInfo = document.getElementById('deleteInfo');
-    let task = document.getElementById('task');
-    let deleteImgContainer = document.getElementById('deleteImgContainer');
-    let trashImg = document.getElementById('deleteTask');
-    if (id == 'addTask') {
+function showTooltip(id, index) {
+    console.log('id:', id); 
+    console.log('addTask:', 'addtask' + index );
+    let addInfo = document.getElementById('addInfo' + index);
+    let deleteInfo = document.getElementById('deleteInfo' + index);
+    let task = document.getElementById('task' + index);
+    let deleteImgContainer = document.getElementById('deleteImgContainer' + index);
+    let trashImg = document.getElementById('deleteTask' + index);
+    if (id == 'addTask' + index) {
+       
+       
         onHoverStyleAddInfo(addInfo, deleteImgContainer);
     } else
-        if (id == 'deleteTask') {
+        if (id == 'deleteTask' + index) {
             onHoverStyleDeleteInfo(deleteInfo, task, deleteImgContainer, trashImg);
         }
 }
 
-function hideTooltip(id) {
-    let addInfo = document.getElementById('addInfo');
-    let deleteInfo = document.getElementById('deleteInfo');
-    let deleteImgContainer = document.getElementById('deleteImgContainer');
-    let task = document.getElementById('task');
-    let trashImg = document.getElementById('deleteTask');
-    if (id == 'addTask') {
+
+function hideTooltip(id, index) {
+    let addInfo = document.getElementById('addInfo' + index);
+    let deleteInfo = document.getElementById('deleteInfo' + index);
+    let deleteImgContainer = document.getElementById('deleteImgContainer' + index);
+    let task = document.getElementById('task' + index);
+    let trashImg = document.getElementById('deleteTask' + index);
+    if (id == 'addTask' + index) {
         onMouseLeaveStyleAddInfo(addInfo, deleteImgContainer);
     } else
-        if (id == 'deleteTask') {
+        if (id == 'deleteTask' + index) {
             onMouseLeaveStyleDeleteInfo(deleteInfo, task, deleteImgContainer, trashImg);
         }
-
 }
+
 
 function onHoverStyleAddInfo(addInfo, deleteImgContainer) {
     addInfo.style.display = "flex";
     deleteImgContainer.style.borderLeft = "none";
-    deleteImgContainer.style.backgroundColor = "rgba(0, 0, 0, 1.1)";
+    deleteImgContainer.style.backgroundColor = "rgba(0, 0, 0, 0.95)";
 }
+
 
 function onHoverStyleDeleteInfo(deleteInfo, task, deleteImgContainer, trashImg) {
     deleteInfo.style.display = "flex";
     deleteInfo.style.color = "red";
     deleteImgContainer.style.border = "1px solid red";
     deleteImgContainer.style.borderLeft = "none";
-    deleteImgContainer.style.backgroundColor = "rgba(0, 0, 0, 1.1)";
+    deleteImgContainer.style.backgroundColor = "rgba(0, 0, 0, 0.95)";
     task.style.border = "1px solid red";
     task.style.borderRight = "none";
     trashImg.src = "./img/trash_icon_red.png";
 }
 
-function  onMouseLeaveStyleAddInfo(addInfo, deleteImgContainer) {
+
+function onMouseLeaveStyleAddInfo(addInfo, deleteImgContainer) {
     addInfo.style.display = "none";
-    deleteImgContainer.style.backgroundColor = "rgba(0, 0, 0, 0.65)";
+    deleteImgContainer.style.backgroundColor = "rgba(0, 0, 0, 0.60)";
 }
 
-function  onMouseLeaveStyleDeleteInfo(deleteInfo, task, deleteImgContainer, trashImg) {
+
+function onMouseLeaveStyleDeleteInfo(deleteInfo, task, deleteImgContainer, trashImg) {
     deleteInfo.style.display = "none";
     deleteImgContainer.style.border = "solid 1px rgb(0, 114, 255)";
     deleteImgContainer.style.borderLeft = "none";
     task.style.border = "1px solid rgb(0, 114, 255)";
     task.style.borderRight = "none";
     trashImg.src = "./img/trash_icon.png";
-    deleteImgContainer.style.backgroundColor = "rgba(0, 0, 0, 0.65)";
+    deleteImgContainer.style.backgroundColor = "rgba(0, 0, 0, 0.60)";
 }
+
 
 async function deleteBacklogTask(position) {
     await deleteTask(position);
